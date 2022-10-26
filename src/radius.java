e4d60b6e7e7a104b5aebed32e91f5cf1ee73d65c
emma <emma@iknowthe.net>
2022-10-26 15:22:52 +0100

    Initial commit

---
+++ src/radius.java
@@ -0,0 +1,16 @@
+public class radius {
+    public static void main(String[] args) {
+        double circleRadius = 12.0;
+
+        double circleCircumference = 2 * (3.14 * circleRadius);
+        double circleDiameter = 2 * (circleRadius);
+        double circleArea = 3.14 * (circleRadius * circleRadius);
+
+        System.out.println("Circumference is: " + circleCircumference);
+        System.out.println("Area is : " + circleArea);
+        System.out.println("Diameter is : " + circleDiameter);
+
+    }
+
+}
+
